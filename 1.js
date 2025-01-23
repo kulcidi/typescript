73369c2aea99d6df8e8156b66d81b5e56456a754
kulcidi <kullciid252@gmail.com>
2025-01-23 09:37:59 +0900

    1 finished

---
+++ 1.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 5);
+// example 
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 8));
+console.log(multiply(1, 2));
---
+++ 1.ts
@@ -0,0 +1,13 @@
+function sum(a: number, b: number) {
+	return a + b
+}
+
+sum(10, 5)
+// example ＞﹏＜
+
+function multiply( a: number, b: number): number {
+   return a * b ;
+}
+
+console.log(multiply(3, 8));
+console.log(multiply(1, 2));
